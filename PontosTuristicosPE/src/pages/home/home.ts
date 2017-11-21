d982ad321f29860adc24f85140df96c89ad82b8e
Ana Carolina <a.caroolinasantos@gmail.com>
2017-11-21 01:47:42 -0300

    search de cidade

--- PontosTuristicosPE/src/pages/home/home.ts
+++ PontosTuristicosPE/src/pages/home/home.ts
@@ -1,6 +1,9 @@
+import { ListaResultadosProvider } from './../../providers/lista-resultados/lista-resultados';
 import { ListaResultadosPage } from './../lista-resultados/lista-resultados';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/debounceTime';
 
 
 @Component({
@@ -8,13 +11,51 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  searchTerm: string = '';
+  searchControl: FormControl;
+  cidades: any;
+  cidadeSelecionada: boolean=false;
+  exibirLista: boolean=true;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public listagemServico: ListaResultadosProvider) {
+    this.searchControl = new FormControl();
 
   }
 
-  abrirLista(id_tipocategoria){
-    this.navCtrl.push(ListaResultadosPage, {id: id_tipocategoria})
+  ionViewDidLoad() {
+
+    this.setFilteredItems();
+    this.searchControl.valueChanges.debounceTime(400).subscribe(search => {
+      if(this.exibirLista==false){
+        this.cidadeSelecionada=false;
+      }
+      if(this.cidadeSelecionada){
+        this.exibirLista=false;
+      }
+      this.setFilteredItems();
+      
+
+    });
+
+  }
+
+  setFilteredItems() {
+
+    this.cidades = this.listagemServico.filterItems(this.searchTerm);
+
+  }
+
+  atualizaCidade(cidade){
+    this.searchTerm=cidade.nome;
+    this.cidadeSelecionada=true;
+
+    if(this.exibirLista==false){
+      this.exibirLista=true;
+    }
+  }
+
+  abrirLista(id_tipocategoria) {
+    this.navCtrl.push(ListaResultadosPage, { id: id_tipocategoria })
   }
 
 }
--- PontosTuristicosPE/src/providers/lista-resultados/lista-resultados.ts
+++ PontosTuristicosPE/src/providers/lista-resultados/lista-resultados.ts
@@ -6,19 +6,46 @@ import 'rxjs/Rx';
 
 
 let apiListaUrl = "http://localhost:8100/api/places/category";
+let apiListaUrlCategoriaCidade = "http://localhost:8100/api/places?category=";
 
 @Injectable()
 export class ListaResultadosProvider {
+  cidades: any;
 
   constructor(public http: Http) {
     console.log('Hello ListaResultadosProvider Provider');
+
+    this.cidades = [
+      {id_cidade: 134, nome: 'Recife'},
+      {id_cidade: 113, nome: 'Olinda'},
+      {id_cidade: 15, nome: 'Arcoverde'},
+      {id_cidade: 32, nome: 'Cabo de Santo Agostinho'},
+      {id_cidade: 46, nome: 'Caruaru'},
+      {id_cidade: 45, nome: 'Carpina'}
+  ]
+
   }
+
+  filterItems(searchTerm){
+    
+           return this.cidades.filter((cidade) => {
+               return cidade.nome.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+           });    
+    
+       }
 
   abrirListaLocais(id) {
     var url = apiListaUrl + "/" + id;
     return this.http.get(url)
       .map(res => res.json())
       .catch(res => { return Observable.throw(res) });
+  }
+
+  abrirListaLocaisCidade(id_categoria, id_cidade){
+    var url = apiListaUrlCategoriaCidade + id_categoria + "&city=" + id_cidade;
+    return this.http.get(url)
+    .map(res => res.json())
+    .catch(res => { return Observable.throw(res) });
   }
 
 }
